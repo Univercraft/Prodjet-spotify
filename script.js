eb1d44f2bca7026a722f350184b280f64a849762
Univercraft <160036881+Univercraft@users.noreply.github.com>
2024-03-18 08:21:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+fetch('data.json'); // requête vers le fichier JSON
+  .then(response => response.json()) //convertir la réponse textuelle en JSON
+  .then(data => {
+    // traiter les données
+    console.log(data);
+    setTrackList(data);
+  });
+
+
+  function setTrackList(data){
+    // récupération du template
+    let template = document.getElementById('trackCard');
+
+    // parcourir les chansons
+    for (let i = 0; i < data.length; i++) {
+        // faire un clone tu template
+        const clone = template.content.cloneNode(true);
+
+        let artists= data[i].artists[0].name;
+
+        // remplir le clone
+        clone.querySelector('.card-title').textContent = data[i].name;
+        clone.querySelector('.card-text').textContent = artists;
+        clone.querySelector('.card-img-top').src = data[i].album.images[0].url;
+        clone.querySelector('.card-img-top').alt = data[i].name;
+
+        // ajouter le clone au DOM dans le conteneur
+        document.getElementById('trackList').appendChild(clone);
+    }
+}
